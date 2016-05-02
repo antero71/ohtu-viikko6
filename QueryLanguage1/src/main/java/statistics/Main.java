0cc5ab7174d8f1677044ce44d58d484a2afc2dc6
Antero Oikkonen <antero.oikkonen@gmail.com>
2016-05-02 21:51:31 +0300

    Or.java

--- QueryLanguage1/src/main/java/statistics/Main.java
+++ QueryLanguage1/src/main/java/statistics/Main.java
@@ -3,27 +3,41 @@ package statistics;
 import statistics.matcher.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
-        );
+
+        System.out.println("Väh: 10 goal, väh 10 assists");
         
+        Matcher m = new And(new HasAtLeast(10, "goals"),
+                new HasAtLeast(10, "assists"),
+                new PlaysIn("PHI")
+        );
+
         for (Player player : stats.matches(m)) {
-            System.out.println( player );
+            System.out.println(player);
         }
-        
-        System.out.println("\n");
-        
-        m = new And( new HasFewerThan(10, "goals"),
-                             new HasFewerThan(10, "assists"),
-                             new PlaysIn("PHI")
+
+        System.out.println("\nEnintään 10 goal, vähintään 10 assists");
+
+        m = new And(new HasFewerThan(10, "goals"),
+                new HasAtLeast(10, "assists"),
+                new PlaysIn("PHI")
         );
-        
+
         for (Player player : stats.matches(m)) {
-            System.out.println( player );
+            System.out.println(player);
+        }
+
+        System.out.println("\nVah: 30 goal, tai Vah: 30 assists");
+
+        m = new And(new Or(new HasAtLeast(30, "goals"),
+                new HasAtLeast(30, "assists")),
+                new PlaysIn("PHI")
+        );
+
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
         }
     }
 }
